6756043a2c96bf6e361ebfe08cf4595ce6ec630a
shpark <shpark@gaia3d.com>
2020-07-24 16:56:01 +0900

    참조키 수정, 불필요 파일 제거

--- doc/database/ddl/constraint/reference.sql
+++ doc/database/ddl/constraint/reference.sql
@@ -1,8 +1,5 @@
 --foreign key 설정 : 식별자는 "(테이블명)_fk_(칼럼명)"으로 통일
 
-alter table only tn_civil_voice add constraint tn_civil_voice_fk_user_id foreign key (user_id) references user_info(user_id);
-alter table only tn_civil_voice_comment add constraint tn_civil_voice_comment_fk_civil_voice_id foreign key (civil_voice_id) references tn_civil_voice(civil_voice_id);
-
 alter table only converter_job add constraint converter_job_fk_upload_data_id foreign key (upload_data_id) references upload_data(upload_data_id);
 alter table only converter_job_file add constraint converter_job_file_fk_converter_job_id foreign key (converter_job_id) references converter_job(converter_job_id);
 
@@ -37,4 +34,9 @@ alter table only user_group_role add constraint user_group_role_fk_user_group_id
 alter table only user_group_role add constraint user_group_role_fk_role_id foreign key (role_id) references role(role_id);
 
 alter table only user_policy add constraint user_policy_fk_user_id foreign key (user_id) references user_info(user_id);
+
+alter table only data_info add constraint data_info_fk_data_group_id foreign key (data_group_id) references data_group(data_group_id);
+alter table only data_file_info add constraint data_file_info_fk_data_group_id foreign key (data_group_id) references data_group(data_group_id);
+alter table only data_file_parse_log add constraint data_file_parse_log_fk_data_file_info_id foreign key (data_file_info_id) references data_file_info(data_file_info_id);
+
 
